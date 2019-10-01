4549d27681284448002c748bd5025d085a94d1f4
Dheeraj Kumar <dheerajk809@gmail.com>
2019-10-01 17:06:20 +0530

    Snake ladder problem solution with recursion

---
+++ TutorialHorizon/Array/SnakeLadderProblem.java
@@ -0,0 +1,56 @@
+/**
+ * SnakeLadderProblem
+ */
+public class SnakeLadderProblem {
+
+    static int minJump = Integer.MAX_VALUE;
+
+    public static void main(String[] args) {
+        int size = 36;
+        int[] board = new int[size];
+        for (int i = 0; i < size; i++) {
+            board[i] = -1;
+        }
+
+        board[2] = 15;
+        board[14] = 24;
+        board[20] = 31;
+
+        // Snakes
+        board[11] = 1;
+        board[29] = 3;
+        board[34] = 21;
+
+        int trackArr[] = new int[size];
+
+        for (int i = 1; i < size; i++) {
+            trackArr[i] = Integer.MAX_VALUE;
+        }
+
+        findMinimumJump(board, 0, trackArr);
+        System.out.println("Minimum Jump: " + trackArr[size - 1]);
+    }
+
+    public static void findMinimumJump(int board[], int current, int trackArr[]) {
+
+        if (current == (board.length - 1)) {
+            return;
+        }
+
+        if (board[current] != -1) {
+            trackArr[board[current]] = trackArr[current];
+            current = board[current];
+        }
+
+        for (int i = 1; i <= 6; i++) {
+            if ((current + i) < trackArr.length) {
+                if ((trackArr[current] + 1) < trackArr[current + i]) {
+                    trackArr[current + i] = trackArr[current] + 1;
+                    findMinimumJump(board, (current + i), trackArr);
+                }
+            }
+        }
+
+    }
+
+}
