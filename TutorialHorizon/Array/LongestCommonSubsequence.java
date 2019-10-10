ad1b097e608193be3a55a7d9c06fbcf02f58717c
Dheeraj Kumar <dheerajk809@gmail.com>
2019-10-10 20:04:59 +0530

    Longest common subsequence solution using DP

--- TutorialHorizon/Array/LongestCommonSubsequence.java
+++ TutorialHorizon/Array/LongestCommonSubsequence.java
@@ -10,12 +10,26 @@ public class LongestCommonSubsequence {
         int arr[][] = new int[s1.length()][s2.length()];
 
         for (int i = 0; i < s1.length(); i++) {
-
+            if (s1.charAt(i) == s2.charAt(0)) {
+                arr[i][0] = 1;
+            }
         }
 
         for (int i = 0; i < s2.length(); i++) {
-
+            if (s1.charAt(0) == s2.charAt(i)) {
+                arr[0][1] = 1;
+            }
         }
 
+        for (int i = 1; i < s1.length(); i++) {
+            for (int j = 1; j < s2.length(); j++) {
+                if (s1.charAt(i) == s2.charAt(j)) {
+                    arr[i][j] = arr[i - 1][j - 1] + 1;
+                } else {
+                    arr[i][j] = Math.max(arr[i - 1][j], arr[i][j - 1]);
+                }
+            }
+        }
+        System.out.println("Max Subsequence length: " + arr[s1.length() - 1][s2.length() - 1]);
     }
 }
